70dd9c37f8b56b07d1a500e74d266c75470b4e0f
lewypopescu <popesculewy@gmail.com>
2024-07-13 08:24:58 +0300

    lifecycle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,27 +9,24 @@ import styles from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  handleAddContact = contact => {
-    const { contacts } = this.state;
-    const contactExists = contacts.some(
-      existingContact =>
-        existingContact.name.toLowerCase() === contact.name.toLowerCase()
-    );
-
-    if (contactExists) {
-      alert(`${contact.name} is already in contacts.`);
-      return;
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
     }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleAddContact = contact => {
     const newContact = {
       id: nanoid(),
       ...contact,
@@ -37,6 +34,12 @@ export class App extends Component {
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
@@ -49,12 +52,6 @@ export class App extends Component {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-  };
-
-  handleDeleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
   };
 
   render() {
